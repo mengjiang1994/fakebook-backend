4659073711d994291bc2a812089a1ad73ca8c6ee
Meng Jiang <mengjiang@uq.net.au>
2019-06-09 10:11:36 +0800

    koa2-cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const Router = require('koa-router');
 const app = new Koa();
-// const cors = require('cors');
+const cors = require('koa2-cors');
 
 // log all events to the terminal
 app.use(logger());
@@ -23,24 +23,14 @@ const router = new Router();
 // require our external routes and pass in the router
 require('./routes/basic')({ router });
 
-app.use('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
-//res.header('Access-Control-Allow-Origin', 'http://www.baidu.com'); //这样写，只有www.baidu.com 可以访问。
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
-  if (req.method == 'OPTIONS') {
-    res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
-  }
-  else {
-    next();
-  }
-});
+app.use(cors());
+
 
 
 app
-   .use(require('koa-body')())
-   .use(router.allowedMethods())
-   .use(router.routes())
+.use(require('koa-body')())
+.use(router.allowedMethods())
+.use(router.routes())
 
 // tell the server to listen to events on a specific port
 const server = app.listen(3005);
